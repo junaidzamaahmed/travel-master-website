ea78c5ce4336213c5e5a933e9c48a2d9cfd55a5c
junaidzamaahmed <junaidahmedzama@gmail.com>
2021-10-31 15:00:20 +0600

    fourth commit

--- src/components/Package/Package.js
+++ src/components/Package/Package.js
@@ -16,7 +16,7 @@ const Package = ({ pkg }) => {
                     <h5 className="card-title fw-bold">{pkg?.title}</h5>
                     <p className="card-text card-description">{pkg?.description}</p>
                     <p className="card-text fw-bold my-3">Cost Per Person: ${pkg?.price}</p>
-                    <div className="d-flex justify-content-center">
+                    <div className="d-flex justify-content-center w-100">
                         <Button
                             className="primary-background button text-light text-decoration-none px-4 py-2 rounded-pill nav-item primary-button text-center"
                             onClick={handleBookClick}
